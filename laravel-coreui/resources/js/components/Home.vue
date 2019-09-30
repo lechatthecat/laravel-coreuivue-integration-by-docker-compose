db41b6bbc21fdc8ffcd73f4c324c4463a6bf344d
lechatthecat <shu845@gmail.com>
2019-09-30 23:07:29 +0900

    fix typo

--- laravel-coreui/resources/js/components/Home.vue
+++ laravel-coreui/resources/js/components/Home.vue
@@ -2,7 +2,7 @@
     <div class="app flex-row align-items-center">
         <ul>
         <li>Login: <a href="/#/login">Login</a></li>
-        <li>Regiaster: <a href="/#/register">Regiaster</a></li>
+        <li>Register: <a href="/#/register">Register</a></li>
         </ul>
     </div>
 </template>>
