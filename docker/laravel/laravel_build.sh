c67d773ea12efbe106a95305a3e40ed356aff2f4
lechatthecat <shu845@gmail.com>
2020-11-12 06:19:11 +0900

    fix bug

--- docker/laravel/laravel_build.sh
+++ docker/laravel/laravel_build.sh
@@ -1,10 +1,10 @@
 #!/bin/ash
 cd /laravel-coreui/src
-composer install
-php artisan key:generate
 chown -R www-data:www-data ../../laravel-coreui/src
 chmod 777 storage
 npm install
 npm run dev 
 composer update
 composer self-update
+composer install
+php artisan key:generate
